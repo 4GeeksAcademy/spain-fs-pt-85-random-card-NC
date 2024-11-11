e408651e9b73897e169ae650c72fcf6d2250ff69
Nachocordoba13 <nachocordoba34@gmail.com>
2024-11-11 23:51:10 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  document.querySelector(".card").classList.add(generateRandomSuit());
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "heart", "club", "spade"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+
+  return suit[indexSuit];
 };
